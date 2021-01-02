1fa9cf35068426f22cd3b3a253b37028e346f75d
shellc0d3 <shellc0d3@protonmail.com>
2021-01-03 00:24:51 +0100

    Add app.js

---
+++ app.js
@@ -0,0 +1,33 @@
+const login = require("facebook-chat-api");
+const fs = require("fs");
+
+login({appState: JSON.parse(fs.readFileSync('appstate.json', 'utf8'))}, (err, api) =>
+{
+        api.setOptions(
+        {
+                selfListen: true,
+                logLevel: 'warn'
+        });
+
+	api.listenMqtt((err, msgReceived) =>
+	{
+		if (msgReceived.type === 'message' && msgReceived.body.indexOf('@everyone', 0) >= 0)
+		{
+			api.getThreadInfo(msgReceived.threadID, (err, info)=>
+			{
+				//This is a work-around for a bug (see facebook-chat-api issue #857)
+				//mention doesn't work if tag is at index zero in body string
+				const emptyChar = '\u200E';
+
+				var msgToSend = { body: emptyChar + 'AAAAAAAAAAAAA', mentions: [] };
+
+				for(var i=0; i< info.participantIDs.length; i++)
+				{
+					msgToSend.mentions.push({tag: 'AAAAAAAAAAAAA', id: info.participantIDs[i]});
+				}
+
+				api.sendMessage(msgToSend, info.threadID);
+			});
+		}
+	});
+});
